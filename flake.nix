ca191be85db1c6a0df855000880ff04c78c2a19e
rawpie <8276063+rawpie2@users.noreply.github.com>
2024-08-20 00:31:19 -0400

    flake

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "nvim flake";
+
+  inputs = {
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = _: {
+    nixosModules = {
+      nvim = import ./nvim.nix;
+    };
+  };
+}
